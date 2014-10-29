335179e17e21c8922a6d6638380b706430453224
Elethom Hunter <elethomhunter@gmail.com>
2014-10-29 17:53:10 +0800

    Initial Commit

---
+++ PRAlertControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PRAlertControllerDemo
+//
+//  Created by Elethom Hunter on 29/10/2014.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PRAlertControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PRAlertControllerDemo
+//
+//  Created by Elethom Hunter on 29/10/2014.
+//  Copyright (c) 2014 Project Rhinestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
